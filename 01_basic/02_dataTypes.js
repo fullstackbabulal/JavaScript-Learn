0ef9f5657a34622d9ce2b760d076b3e6fff76bb4
fullstackbabulal <fullstack.babulal@gmail.com>
2024-06-03 21:50:32 +0530

    Data Types Ki Kahani

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict";
+/* JavaScript is dynamically typed, meaning that a variable can change its data type during runtime.
+JavaScript, there are several data types that you can work with. */
+
+/* 
+Understanding JavaScript's data types is crucial as it forms the foundation of programming in JavaScript. There are eight fundamental data types: string, number, boolean, null, undefined, symbol, and object.
+*/
+
+let userName = "Babulal Mandal"; // String
+let yourAge = 27; // Number
+let isUserLogedin = false; // boolean
+let yourHealthScore = null; // null
+let yourAddress; // undefined
+let symbolOfCode = Symbol(); // symbol
+let myIdentity = {name:"Babulal", age:27,};//object
+let bigNumber = BigInt("154749494144449746944555"); //BigInt
+
+/* 
+Primitive Data Types:
+String: Represents a sequence of characters, such as "Hello, world!".
+Number: Represents numeric values, both integers and floating-point numbers.
+Boolean: Represents true or false values.
+Undefined: Indicates that a variable has been declared but hasn’t been assigned a value yet.
+Null: Represents the intentional absence of any value.
+Composite Data Types:
+Object: A complex data type that can hold key-value pairs. Objects can be user-defined or built-in (e.g., arrays, dates, maps).
+Array: An ordered collection of values, accessible by their index (e.g., ["apple", "banana", "cherry"]).
+Function: A reusable block of code that performs a specific task.
+Special Data Types:
+Symbol: A unique and immutable value used as an identifier for object properties.
+BigInt: Represents large integers beyond the range of regular numbers (e.g., 1234567890123456789012345678901234567890n).
+*/
